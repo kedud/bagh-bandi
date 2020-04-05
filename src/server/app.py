54537dfc24932af04e1469efadec41051f9e470d
Come Turrata <come@getluko.com>
2020-04-05 22:20:33 +0200

    Update server

--- src/server/app.py
+++ src/server/app.py
@@ -4,50 +4,90 @@ sys.path.insert(0, os.getcwd())
 from flask import Flask
 from src.domain.engine import Engine
 import json
+import flask
 
 app = Flask(__name__)
 
 engine = Engine()
 
-@app.route('/state', methods=['GET', 'POST'])
+@app.route('/state', methods=['GET', 'POST', 'OPTIONS'])
 def state():
     ret = {}
     ret["goat_positions"] = engine.board.goatsPositions
     ret["tigers_positions"] = engine.board.tigerPositions
     ret["turn"] = engine.board.turn
     ret["re_capture_allowed"] = engine.re_capture_allowed
-    return json.dumps(ret)
+
+    response = flask.jsonify(ret)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', '*')
+    return response
+
 
 @app.route('/move/<player_type>/<int:departure>/<int:destination>', methods=['GET', 'POST'])
 def postId(player_type, departure, destination):
+    msg = "OK"
     if player_type == engine.board.turn:
         if engine.is_valid_move(departure, destination):
             engine.move(departure, destination)
-            return "OK"
         else:
-            return(f"/!\\ INVALID MOVE : {departure} to {destination}")
+            msg = f"/!\\ INVALID MOVE : {departure} to {destination}"
     else:
-        return "NOT YOUR TURN"
+        msg = "NOT YOUR TURN"
+
+    ret = {}
+    ret["msg"] = msg
+    ret["goat_positions"] = engine.board.goatsPositions
+    ret["tigers_positions"] = engine.board.tigerPositions
+    ret["turn"] = engine.board.turn
+    ret["re_capture_allowed"] = engine.re_capture_allowed
+    response = flask.jsonify(ret)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', '*')
+    return response
+
 
 
 @app.route('/reset', methods=['GET', 'POST'])
 def reset():
     global engine
     engine = Engine()
-    return "OK"
+
+    msg = "OK"
+
+    ret = {}
+    ret["msg"] = msg
+    ret["goat_positions"] = engine.board.goatsPositions
+    ret["tigers_positions"] = engine.board.tigerPositions
+    ret["turn"] = engine.board.turn
+    ret["re_capture_allowed"] = engine.re_capture_allowed
+    response = flask.jsonify(ret)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', '*')
+    return response
+
 
 
 @app.route('/skip/<player_type>', methods=['GET', 'POST'])
 def skip_turn(player_type):
     global engine
-    engine = Engine()
-    if engine.re_capture_allowed() and engine.board.turn == player_type and player_type == "tigers":
+    msg = "OK"
+    if engine.re_capture_allowed and engine.board.turn == player_type and player_type == "tigers":
         engine.skip_tiger_recapture()
-        return "OK"
+        msg = "OK"
     else:
-        return "Cannot skip turn"
+        msg = "Cannot skip turn"
 
-
+    ret = {}
+    ret["msg"] = msg
+    ret["goat_positions"] = engine.board.goatsPositions
+    ret["tigers_positions"] = engine.board.tigerPositions
+    ret["turn"] = engine.board.turn
+    ret["re_capture_allowed"] = engine.re_capture_allowed
+    response = flask.jsonify(ret)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', '*')
+    return response
 
 def main():
     app.run(host='0.0.0.0', port=5000)
